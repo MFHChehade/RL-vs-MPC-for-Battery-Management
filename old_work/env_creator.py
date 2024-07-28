28f96fe73266e452cb52012f6d4441769b53624b
Mohamad Fares El Hajj Chehade <122603197+MFHChehade@users.noreply.github.com>
2024-07-28 15:49:45 -0600

    Delete old_work directory

--- old_work/env_creator.py
+++
@@ -1,4 +0,0 @@
-from energy_management_env import EnergyManagementEnv
-
-def energy_management_env_creator(SOC_min, SOC_max, E, lambda_val, data_path, initial_SOC=None):
-    return EnergyManagementEnv(SOC_min, SOC_max, E, lambda_val, data_path, initial_SOC)
